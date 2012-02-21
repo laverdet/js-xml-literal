laverdet/js-xml-literal

commit 96403dd0adc7cf015e2e1ff74d83188f52956fbe

Remove seperate data classes
I originally had those because I wanted to make DOM nodes appear opaque
to the user but I realized there's a non-insignificant performance
penalty for that. So the classes are going.

lib/simple-dom/unescaped-data.js
@@ -12,9 +12,7 @@ var CharacterDataData = characterData.CharacterDataData;
  * Raw markup in an XML tree. Danger!
  */
 function UnescapedData(data) {
-	Object.defineProperty(this, '__', {
-		value: new CharacterDataData(data),
-	});
+	this.__ = new CharacterDataData(data);
 }
 extend(UnescapedData, CharacterData);
 

lib/simple-dom/character-data.js
@@ -1,5 +1,4 @@
 this.CharacterData = CharacterData;
-this.CharacterDataData = CharacterDataData;
 
 var util = require('./util');
 var node = require('./node');
@@ -14,50 +13,43 @@ var NodeData = node.NodeData;
  * Abstract `CharacterData` node. This is for `Text`, and `Comment` nodes.
  */
 function CharacterData(data) {
-	Object.defineProperty(this, '__', {
-		value: new CharacterDataData(data),
-	});
+	Node.call(this);
+	this.__data = data;
 }
 extend(CharacterData, Node);
 
-function CharacterDataData(data) {
-	NodeData.call(this);
-	this.data = data;
-}
-extend(CharacterDataData, NodeData);
-
 defineProperties(CharacterData.prototype, {
 	appendData: function(data) {
-		this.__.data += data;
+		this.__data += data;
 	},
 
 	deleteData: function(offset, length) {
-		this.__.data = data.substr(0, offset) + data.substr(offset + length);
+		this.__data = data.substr(0, offset) + data.substr(offset + length);
 	},
 
 	insertData: function(offset, data) {
-		this.__.data = data.substr(0, offset) + data + data.substr(offset);
+		this.__data = data.substr(0, offset) + data + data.substr(offset);
 	},
 
 	replaceData: function(offset, length, data) {
-		this.__.data = data.substr(0, offset) + data + data.substr(offset + length);
+		this.__data = data.substr(0, offset) + data + data.substr(offset + length);
 	},
 
 	substringData: function(offset, length) {
-		return this.__.data.substr(offset, length);
+		return this.__data.substr(offset, length);
 	},
 });
 
 defineGetters(CharacterData.prototype, {
 	data: function() {
-		return this.__.data;
+		return this.__data;
 	},
 
 	length: function() {
-		return this.__.data.length;
+		return this.__data.length;
 	},
 
 	nodeValue: function() {
-		return this.__.data;
+		return this.__data;
 	}
 });

lib/simple-dom/element.js
@@ -1,5 +1,4 @@
 this.Element = Element;
-this.ElementData = ElementData;
 
 var util = require('./util');
 var environment = require('../environment');
@@ -13,7 +12,6 @@ var defineProperties = util.defineProperties;
 var defineGetters = util.defineGetters;
 var XMLEnvironment = environment.XMLEnvironment;
 var NodeWithChildren = nodeWithChildren.NodeWithChildren;
-var NodeWithChildrenData = nodeWithChildren.NodeWithChildrenData;
 
 /**
  * Creates a string of indentation
@@ -30,20 +28,13 @@ function indent(level) {
  * `Element` node. Represents a named element.
  */
 function Element(nodeName, namespaceURI, attributes, attributesNS) {
-	Object.defineProperty(this, '__', {
-		value: new ElementData(nodeName, namespaceURI, attributes, attributesNS),
-	});
+	NodeWithChildren.call(this);
+	this.__attributes = attributes;
+	this.__attributesNS = attributesNS;
+	this.__namespaceURI = namespaceURI;
+	this.__nodeName = nodeName;
 }
 extend(Element, NodeWithChildren);
-
-function ElementData(nodeName, namespaceURI, attributes, attributesNS) {
-	NodeWithChildrenData.call(this);
-	this.attributes = attributes;
-	this.attributesNS = attributesNS;
-	this.namespaceURI = namespaceURI;
-	this.nodeName = nodeName;
-}
-extend(ElementData, NodeWithChildrenData);
 
 Object.defineProperty(Element.prototype, '_selfClosing', {
 	value: true,
@@ -67,35 +58,35 @@ function resolvePrefix(uri, namespaces) {
 function elementToString(el, namespaces, usedNamespaces, pretty, level, first) {
 	// open tag
 	var str = '<';
-	if (namespaces[el.__.namespaceURI] !== '') {
-		usedNamespaces[el.__.namespaceURI] = true;
-		str += resolvePrefix(el.__.namespaceURI, namespaces) + ':';
+	if (namespaces[el.__namespaceURI] !== '') {
+		usedNamespaces[el.__namespaceURI] = true;
+		str += resolvePrefix(el.__namespaceURI, namespaces) + ':';
 	}
-	str += el.__.nodeName;
+	str += el.__nodeName;
 
 	// attributes
-	for (var ii in el.__.attributes) {
+	for (var ii in el.__attributes) {
 		var val;
-		if (el.__.attributes[ii] === true) {
+		if (el.__attributes[ii] === true) {
 			val = ii;
-		} else if (el.__.attributes[ii] === false) {
+		} else if (el.__attributes[ii] === false) {
 			continue;
 		} else {
-			val = escapeAttributeValue(ToString(el.__.attributes[ii]));
+			val = escapeAttributeValue(ToString(el.__attributes[ii]));
 		}
 		str += ' ' + ii + '="' + val + '"';
 	}
 
 	// namespaced attributes
-	for (var uri in el.__.attributesNS) {
-		for (var ii in el.__.attributesNS[uri]) {
+	for (var uri in el.__attributesNS) {
+		for (var ii in el.__attributesNS[uri]) {
 			var val;
-			if (el.__.attributesNS[uri][ii] === true) {
+			if (el.__attributesNS[uri][ii] === true) {
 				val = ii;
-			} else if (el.__.attributesNS[uri][ii] === false) {
+			} else if (el.__attributesNS[uri][ii] === false) {
 				continue;
 			} else {
-				val = escapeAttributeValue(ToString(el.__.attributesNS[uri][ii]));
+				val = escapeAttributeValue(ToString(el.__attributesNS[uri][ii]));
 			}
 			usedNamespaces[uri] = true;
 			str += ' ' + resolvePrefix(uri, namespaces) + ':' + ii + '="' + val + '"';
@@ -104,21 +95,21 @@ function elementToString(el, namespaces, usedNamespaces, pretty, level, first) {
 
 	// children (must render these first to figure out which namespaces were found)
 	var content = '';
-	for (var ii = 0; ii < el.__.childNodes.length; ++ii) {
+	for (var ii = 0; ii < el.__childNodes.length; ++ii) {
 		if (pretty) {
-			if (el.__.childNodes.length || el.__.childNodes[0] instanceof Element) {
+			if (el.__childNodes.length || el.__childNodes[0] instanceof Element) {
 				content += '\n';
 				content += indent(level + 1);
 			}
 		}
-		if (el.__.childNodes[ii] instanceof Element) {
+		if (el.__childNodes[ii] instanceof Element) {
 			content +=
-				elementToString(el.__.childNodes[ii], namespaces, usedNamespaces, pretty, level + 1);
+				elementToString(el.__childNodes[ii], namespaces, usedNamespaces, pretty, level + 1);
 		} else {
-			content += el.__.childNodes[ii].toString();
+			content += el.__childNodes[ii].toString();
 		}
 	}
-	if (pretty && (el.__.childNodes.length || el.__.childNodes[0] instanceof Element)) {
+	if (pretty && (el.__childNodes.length || el.__childNodes[0] instanceof Element)) {
 		content += '\n';
 	}
 
@@ -130,10 +121,10 @@ function elementToString(el, namespaces, usedNamespaces, pretty, level, first) {
 	}
 
 	// self-closing tag with no children
-	if (el._selfClosing && el.__.childNodes.length === 0) {
+	if (el._selfClosing && el.__childNodes.length === 0) {
 		return str + '/>';
 	}
-	return str + '>' + content + '</' + el.__.nodeName + '>';
+	return str + '>' + content + '</' + el.__nodeName + '>';
 }
 
 function normalizeName(name) {
@@ -146,11 +137,11 @@ function normalizeName(name) {
 
 defineProperties(Element.prototype, {
 	getAttribute: function(attr) {
-		return this.__.attributes[normalizeName(attr)];
+		return this.__attributes[normalizeName(attr)];
 	},
 
 	setAttribute: function(attr, val) {
-		this.__.attributes[normalizeName(attr)] = val;
+		this.__attributes[normalizeName(attr)] = val;
 	},
 
 	getAttributeNS: function(uri, attr) {
@@ -158,8 +149,8 @@ defineProperties(Element.prototype, {
 		if (uri === '') {
 			return this.getAttribute(attr);
 		}
-		if (this.__.attributesNS && (uri in this.__.attributesNS)) {
-			return this.__.attributesNS[uri][normalizeName(attr)];
+		if (this.__attributesNS && (uri in this.__attributesNS)) {
+			return this.__attributesNS[uri][normalizeName(attr)];
 		}
 		return undefined;
 	},
@@ -169,13 +160,13 @@ defineProperties(Element.prototype, {
 		if (uri === '') {
 			return this.setAttribute(attr, val);
 		}
-		if (!this.__.attributesNS) {
-			this.__.attributesNS = {};
+		if (!this.__attributesNS) {
+			this.__attributesNS = {};
 		}
-		if (!(uri in this.__.attributesNS)) {
-			this.__.attributesNS[uri] = {};
+		if (!(uri in this.__attributesNS)) {
+			this.__attributesNS[uri] = {};
 		}
-		this.__.attributesNS[uri][normalizeName(attr)] = val;
+		this.__attributesNS[uri][normalizeName(attr)] = val;
 	},
 
 	toString: function() {
@@ -186,14 +177,14 @@ defineProperties(Element.prototype, {
 
 defineGetters(Element.prototype, {
 	nodeName: function() {
-		return this.__.nodeName;
+		return this.__nodeName;
 	},
 
 	namespaceURI: function() {
-		return this.__.namespaceURI;
+		return this.__namespaceURI;
 	},
 
 	tagName: function() {
-		return this.__.nodeName;
+		return this.__nodeName;
 	},
 });

lib/simple-dom/fragment.js
@@ -6,29 +6,23 @@ var nodeWithChildren = require('./node-with-children');
 var extend = util.extend;
 var defineProperties = util.defineProperties;
 var NodeWithChildren = nodeWithChildren.NodeWithChildren;
-var NodeWithChildrenData = nodeWithChildren.NodeWithChildrenData;
 
 /**
  * Document fragment.
  */
 function Fragment() {
-	Object.defineProperty(this, '__', {
-		value: new NodeWithChildrenData(),
-		writeable: false,
-		configurable: false,
-		enumerable: false
-	});
+	NodeWithChildren.call(this);
 }
 extend(Fragment, NodeWithChildren);
 
 defineProperties(Fragment.prototype, {
 	toString: function(pretty) {
 		var str = '';
-		for (var ii = 0; ii < this.__.childNodes.length; ++ii) {
+		for (var ii = 0; ii < this.__childNodes.length; ++ii) {
 			if (pretty && ii) {
 				str += '\n';
 			}
-			str += this.__.childNodes[ii].toString(pretty);
+			str += this.__childNodes[ii].toString(pretty);
 		}
 		return str;
 	},

lib/simple-dom/node-with-children.js
@@ -1,5 +1,4 @@
 this.NodeWithChildren = NodeWithChildren;
-this.NodeWithChildrenData = NodeWithChildrenData;
 
 var util = require('./util');
 var node = require('./node');
@@ -9,7 +8,6 @@ var defineProperties = util.defineProperties;
 var defineGetters = util.defineGetters;
 var beget = util.beget;
 var Node = node.Node;
-var NodeData = node.NodeData;
 var nodeIndex = util.nodeIndex;
 
 /**
@@ -20,7 +18,7 @@ function assertValidChild(node, child) {
 		if (child === node) {
 			throw new Error('cannot create circular heirarchy');
 		}
-		node = node.__.parentNode;
+		node = node.__parentNode;
 	}
 }
 
@@ -35,13 +33,13 @@ function assertValidChildren(node, children) {
  * children to an empty array.
  */
 function stealFragmentChildren(node, fragment) {
-	resetChildren(node, fragment.__.childNodes);
-	fragment.__.childNodes = [];
+	resetChildren(node, fragment.__childNodes);
+	fragment.__childNodes = [];
 }
 
 function resetChildren(node, childNodes) {
 	for (var ii = childNodes.length; ii--; ) {
-		childNodes[ii].__.parentNode = node;
+		childNodes[ii].__parentNode = node;
 	}
 }
 
@@ -49,14 +47,10 @@ function resetChildren(node, childNodes) {
  * `NodeWithChildren` node. Subclasses of this may contain children.
  */
 function NodeWithChildren() {
+	Node.call(this);
+	this.__childNodes = [];
 }
 extend(NodeWithChildren, Node);
-
-function NodeWithChildrenData() {
-	NodeData.call(this);
-	this.childNodes = [];
-}
-extend(NodeWithChildrenData, NodeData);
 
 defineProperties(NodeWithChildren.prototype, {
 	insertBefore: function(newChild, refChild) {
@@ -64,13 +58,13 @@ defineProperties(NodeWithChildren.prototype, {
 			return this.appendChild(newChild);
 		}
 		var index = nodeIndex(this, refChild);
-		var childNodes = this.__.childNodes;
+		var childNodes = this.__childNodes;
 		if (newChild instanceof Fragment) {
-			assertValidChildren(this, newChild.__.childNodes);
+			assertValidChildren(this, newChild.__childNodes);
 			if (index === -1) {
-				childNodes.unshift.apply(childNodes, newChild.__.childNodes);
+				childNodes.unshift.apply(childNodes, newChild.__childNodes);
 			} else {
-				childNodes.splice.apply(childNodes, [index, 0].concat(newChild.__.childNodes));
+				childNodes.splice.apply(childNodes, [index, 0].concat(newChild.__childNodes));
 			}
 			stealFragmentChildren(this, newChild);
 		} else if (newChild instanceof Array) {
@@ -88,7 +82,7 @@ defineProperties(NodeWithChildren.prototype, {
 			} else {
 				childNodes.splice(index, 0, newChild);
 			}
-			newChild.__.parentNode = this;
+			newChild.__parentNode = this;
 		}
 	},
 
@@ -98,22 +92,22 @@ defineProperties(NodeWithChildren.prototype, {
 			throw new Error('`oldChild` not found');
 		}
 		if (newChild instanceof Fragment) {
-			assertValidChildren(this, newChild.__.childNodes);
-			this.__.childNodes.splice.apply(this.__.childNodes, [index, 1].concat(newChild.__.childNodes));
+			assertValidChildren(this, newChild.__childNodes);
+			this.__childNodes.splice.apply(this.__childNodes, [index, 1].concat(newChild.__childNodes));
 			stealFragmentChildren(this, newChild);
 		} else if (newChild instanceof Array) {
 			assertValidChildren(this, newChild);
-			this.__.childNodes.splice(index, 1, newChild);
+			this.__childNodes.splice(index, 1, newChild);
 			resetChildren(this, newChild);
 		} else {
 			assertValidChild(this, newChild);
-			if (newChild.__.parentNode) {
-				newChild.__.parentNode.removeChild(newChild);
+			if (newChild.__parentNode) {
+				newChild.__parentNode.removeChild(newChild);
 			}
-			this.__.childNodes[index] = newChild;
-			newChild.__.parentNode = this;
+			this.__childNodes[index] = newChild;
+			newChild.__parentNode = this;
 		}
-		oldChild.__.parentNode = null;
+		oldChild.__parentNode = null;
 		return oldChild;
 	},
 
@@ -122,30 +116,30 @@ defineProperties(NodeWithChildren.prototype, {
 		if (index === -1) {
 			throw new Error('`oldChild` not found');
 		}
-		this.__.childNodes.splice(index, 1);
-		oldChild.__.parentNode = null;
-		newChild.__.parentNode = this;
+		this.__childNodes.splice(index, 1);
+		oldChild.__parentNode = null;
+		newChild.__parentNode = this;
 		return oldChild;
 	},
 
 	appendChild: function(newChild) {
 		if (newChild instanceof Fragment) {
-			assertValidChildren(this, newChild.__.childNodes);
-			this.__.childNodes.push.apply(this.__.childNodes, newChild.__.childNodes);
+			assertValidChildren(this, newChild.__childNodes);
+			this.__childNodes.push.apply(this.__childNodes, newChild.__childNodes);
 			stealFragmentChildren(this, newChild);
 		} else if (newChild instanceof Array) {
 			assertValidChildren(this, newChild);
-			this.__.childNodes.push.apply(this.__.childNodes, Array.prototype.slice.call(newChild));
+			this.__childNodes.push.apply(this.__childNodes, Array.prototype.slice.call(newChild));
 			resetChildren(this, newChild);
 		} else {
 			assertValidChild(this, newChild);
-			this.__.childNodes.push(newChild);
-			newChild.__.parentNode = this;
+			this.__childNodes.push(newChild);
+			newChild.__parentNode = this;
 		}
 	},
 
 	hasChildNodes: function() {
-		return this.__.childNodes.length !== 0;
+		return this.__childNodes.length !== 0;
 	},
 
 	cloneNode: function(deep) {
@@ -156,25 +150,25 @@ defineProperties(NodeWithChildren.prototype, {
 defineGetters(NodeWithChildren.prototype, {
 	childNodes: function() {
 		// Worth it? Just prevents mutation, but slows iteration
-		if (!this.__.childNodeList) {
-			Object.freeze(this.__.childNodeList = beget(this.__.childNodes));
+		if (!this.__childNodeList) {
+			Object.freeze(this.__childNodeList = beget(this.__childNodes));
 		}
-		return this.__.childNodeList;
+		return this.__childNodeList;
 	},
 
 	firstChild: function() {
-		if (this.__.childNodes.length === 0) {
+		if (this.__childNodes.length === 0) {
 			return null;
 		} else {
-			return this.__.childNodes[0];
+			return this.__childNodes[0];
 		}
 	},
 
 	lastChild: function() {
-		if (this.__.childNodes.length === 0) {
+		if (this.__childNodes.length === 0) {
 			return null;
 		} else {
-			return this.__.childNodes[this.__.childNodes.length - 1];
+			return this.__childNodes[this.__childNodes.length - 1];
 		}
 	},
 });

lib/simple-dom/node.js
@@ -1,5 +1,4 @@
 this.Node = Node;
-this.NodeData = NodeData;
 
 var util = require('./util');
 
@@ -11,10 +10,7 @@ var nodeIndex = util.nodeIndex;
  * Abstract `Node` class.
  */
 function Node() {
-}
-
-function NodeData() {
-	this.parentNode = null;
+	this.__parentNode = null;
 }
 
 var stubs = [
@@ -41,18 +37,18 @@ for (var ii = 0; ii < properties.length; ++ii) {
 
 defineGetters(Node.prototype, {
 	parentNode: function() {
-		return this.__.parentNode;
+		return this.__parentNode;
 	},
 
 	previousSibling: function() {
-		var parentNode = this.__.parentNode;
+		var parentNode = this.__parentNode;
 		if (!parentNode) {
 			return null;
 		}
 		var index = nodeIndex(parentNode, this) - 1;
 		if (index > 0) {
-			var sibling = parentNode.__.childNodes[index];
-			sibling.__.index = index;
+			var sibling = parentNode.__childNodes[index];
+			sibling.__index = index;
 			return sibling;
 		} else {
 			return null;
@@ -60,14 +56,14 @@ defineGetters(Node.prototype, {
 	},
 
 	nextSibling: function() {
-		var parentNode = this.__.parentNode;
+		var parentNode = this.__parentNode;
 		if (!parentNode) {
 			return null;
 		}
 		var index = nodeIndex(parentNode, this) + 1;
-		if (index < parentNode.__.childNodes.length) {
-			var sibling = parentNode.__.childNodes[index];
-			sibling.__.index = index;
+		if (index < parentNode.__childNodes.length) {
+			var sibling = parentNode.__childNodes[index];
+			sibling.__index = index;
 			return sibling;
 		} else {
 			return null;

lib/simple-dom/text.js
@@ -7,20 +7,17 @@ var extend = util.extend;
 var escapeElementValue = util.escapeElementValue;
 var defineProperties = util.defineProperties;
 var CharacterData = characterData.CharacterData;
-var CharacterDataData = characterData.CharacterDataData;
 
 /**
  * `Text` node. For text in the DOM.
  */
 function Text(data) {
-	Object.defineProperty(this, '__', {
-		value: new CharacterDataData(data),
-	});
+	CharacterData.call(this, data);
 }
 extend(Text, CharacterData);
 
 defineProperties(Text.prototype, {
 	toString: function() {
-		return escapeElementValue(this.__.data);
+		return escapeElementValue(this.__data);
 	},
 });

lib/simple-dom/util.js
@@ -63,12 +63,12 @@ function defineGetters(obj, getters) {
  * Gets the index of a child with respect to its parent's childNodes
  */
 function nodeIndex(parentNode, node) {
-	var index = node.__.index;
-	if (index === undefined || parentNode.__.childNodes[index] !== node) {
+	var index = node.__index;
+	if (index === undefined || parentNode.__childNodes[index] !== node) {
 		if (index === -1) {
 			return index;
 		}
-		return node.__.index = parentNode.__.childNodes.indexOf(node);
+		return node.__index = parentNode.__childNodes.indexOf(node);
 	}
 	return index;
 }

commit fb46b4a64e50896f8ebdc5c3eb9cf575ccda8bc7

Fix UnescapedData class
96403dd0 broke the UnescapedData class. This fixes it.

lib/simple-dom/unescaped-data.js
@@ -6,18 +6,17 @@ var characterData = require('./character-data');
 var defineProperties = util.defineProperties;
 var extend = util.extend;
 var CharacterData = characterData.CharacterData;
-var CharacterDataData = characterData.CharacterDataData;
 
 /**
  * Raw markup in an XML tree. Danger!
  */
 function UnescapedData(data) {
-	this.__ = new CharacterDataData(data);
+	CharacterData.call(this, data);
 }
 extend(UnescapedData, CharacterData);
 
 defineProperties(UnescapedData.prototype, {
 	toString: function() {
-		return this.__.data;
+		return this.__data;
 	},
 });